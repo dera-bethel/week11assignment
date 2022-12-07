2ff9707d070e6dcf54f06adfbd3a0e22b9eb0255
dera-bethel <betheldera17@gmail.com>
2022-12-07 08:48:17 -0800

    javascriptassignemt

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # week11assignment
+my first javascript
---
+++ index.js
@@ -0,0 +1,4 @@
+let userName = prompt("what is your name?");
+let age = prompt("How old are you?");
+
+alert("welcome," + " " + userName + " " + "you are"+ " " + age + " " + "years old.")
